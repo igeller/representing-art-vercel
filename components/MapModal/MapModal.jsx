ec4aa3fbc2c6473c7d420ba5fba1a42c0081380d
IG <ieg23@miami.edu>
2021-02-21 14:56:14 -0500

    styled the map modal

--- components/MapModal/MapModal.jsx
+++ components/MapModal/MapModal.jsx
@@ -3,46 +3,62 @@ import {useState} from "react";
 import PropTypes from "prop-types";
 import {Button, Modal, ModalDialog} from "react-bootstrap";
 import Link from "next/link";
-import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+import ReactMapboxGl, { Layer, Feature, Marker } from 'react-mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import styles from '../InfoModal/InfoModal.module.scss'
+import styles from './MapModal.module.scss'
+import {faMapMarkerAlt} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";//CHANGE FOR THE DE
 
-//CHANGE FOR THE DE
-
-const InfoModal = (props) => {
+const MapModal = (props) => {
 
     const Map = ReactMapboxGl({
         accessToken:
-            'pk.eyJ1IjoicmVwcmVzZW50aW5nYXJ0IiwiYSI6ImNrazFrcnVoczBzNHgydXQxOWFyZTkyZWIifQ.tkwUtgPIhfaA0S-qOKjkQw'
+            'pk.eyJ1IjoicmVwcmVzZW50aW5nYXJ0IiwiYSI6ImNrazFrcnVoczBzNHgydXQxOWFyZTkyZWIifQ.tkwUtgPIhfaA0S-qOKjkQw',
+        interactive: false,
+        logoPosition: 'top-right',
+        attributionControl: false
     });
+
+
 
     return(
         <>
 
             <Modal
-                className={`bg-transparent h-100`}
+                className={`bg-transparent`}
                 dialogClassName={`${styles.pos}`}
-                contentClassName={`border-0 bg-transparent`}
+                contentClassName={`border-0`}
                 backdropClassName={'bg-transparent'}
+                scrollable={true}
                 animation={false}
                 show={props.openMapModal}
                 onHide={props.closeMapModal}
             >
                 <Modal.Body
-                    className={`p-3`}
+                    className={`h-25 p-3`}
                     style={{backgroundColor: `${props.bgColor}`}}
                 >
-                    <Map
-                        style="mapbox://styles/mapbox/streets-v9"
-                        containerStyle={{
-                            height: '50vh',
-                            width: '100vw'
-                        }}
-                        className={"mw-100"}
+
+                    <Map style="mapbox://styles/mapbox/streets-v9"
+                         containerStyle={{
+                            height: '100%'
+                         }}
+                         className={"w-100"}
+                         center={[-80.191788, 25.761681]}
+                         zoom={[5]}
                     >
-                        <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
-                            <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
-                        </Layer>
+
+                        {/* Circle example */}
+                        <Marker
+                            coordinates={[-80.191788, 25.761681]}
+                            anchor="bottom"
+                            className={`bg-transparent`}
+                            color={'red'}
+                        >
+                            <FontAwesomeIcon icon={faMapMarkerAlt} size={"3x"} className={`${styles.mapIcon}`}/>
+                            {/*<FontAwesomeIcon icon={['fa', 'coffee']} size={"3x"} />*/}
+
+                        </Marker>
                     </Map>
                 </Modal.Body>
 
@@ -51,9 +67,9 @@ const InfoModal = (props) => {
     )
 };
 
-InfoModal.propTypes = {
+MapModal.propTypes = {
     openMapModal: PropTypes.bool.isRequired,
     closeMapModal: PropTypes.func.isRequired
 }
 
-export default InfoModal;
+export default MapModal;
