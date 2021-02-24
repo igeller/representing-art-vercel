9b633be079aadba728da040e6496f27d116c9b08
IG <ieg23@miami.edu>
2021-02-24 00:05:35 -0500

    cleaned up the file where the readjust happens. still works!

--- pages/index.jsx
+++ pages/index.jsx
@@ -21,17 +21,14 @@ const IMAGES = [
 
 
 export default function Home(pageProps) {
-    // const topBan = React.useRef(null);
 
     let [pos, setState] = useState(0)
 
-let yPos = 0;
+    let yPos = 0;
     useEffect(()=>{
 
         console.log('111111', document.readyState)
 
-
-        //
         if(document.readyState != "complete"){
             slow()
         }
@@ -51,13 +48,7 @@ let yPos = 0;
         const timer = setTimeout(() => {
             setState({pos: document.getElementById(`t1`).getBoundingClientRect()})
             console.log('This will run after 2 second!')
-            // console.log('******!!!! complete', document.getElementById('t1').getBoundingClientRect())
-            console.log('This will run after 2 second!')
             return;
-            // p = pos.bottom
-            // const style = {
-            //     // marginTop: `${p}px`
-            //
 
         }, 2000);
         return () => {
@@ -67,93 +58,41 @@ let yPos = 0;
         }
     }
 
-    const [imgsLoaded, setImgsLoaded] = useState(false)
 
-    // useEffect(() => {
-    //     const loadImage = image => {
-    //         return new Promise((resolve, reject) => {
-    //             const loadImg = new Image()
-    //             loadImg.src = image.url
-    //             // wait 2 seconds to simulate loading time
-    //             loadImg.onload = () =>
-    //                 setTimeout(() => {
-    //                     resolve(image.url)
-    //                 }, 2000)
-    //
-    //             loadImg.onerror = err => reject(err)
-    //         })
-    //     }
-    //
-    //     Promise.all(IMAGES.map(image => loadImage(image)))
-    //         .then(() => {setImgsLoaded(true);
-    //             console.log(image.height);}
-    //         )
-    //         .catch(err => console.log(`Failed to load images ${IMAGES[0].lM}`, err))
-    // }, [])
-
-
-
-
-    useLayoutEffect(()=>{
-        console.log('use layouteffectloaded')
-
-        // console.log('******!!!!', document.getElementById('t1').getBoundingClientRect())
-        // if(screen.width >= 900){
-        //     document.getElementsByClassName('loweLogo').item(0).setAttribute('src', 'https://github.com/igeller/assets/blob/main/Assets/Kiosk/Logos/Africa-Kiosk-Logo-Lowe.png?raw=true')
-        //
-        //     document.getElementsByClassName('mellonLogo').item(0).setAttribute('src', "https://github.com/igeller/assets/blob/main/Assets/Kiosk/Logos/Africa-Kiosk-Logo-Mellon.png?raw=true");
-        // }else{
-        //     document.getElementsByClassName('loweLogo').item(0).setAttribute('src', "");
-        //     document.getElementsByClassName('mellonLogo').item(0).setAttribute('src',"https://github.com/igeller/assets/blob/main/Assets/Mobile/Logos/Africa-Kiosk-Mobile-Mellon.png?raw=true");
-        // }
-
-
-        // console.log(screen.width)
-        // console.log('222222', document.readyState)
-        // // const doc = document;
-        // // topBannerBottom = doc.getElementById('t1').getBoundingClientRect().bottom;
-        // // console.log('****', topBannerBottom);
-        // topBannerBottom = document.getElementById('t1').getBoundingClientRect().bottom;
-        // console.log('****', document.getElementById('t1').innerHTML);
-        // console.log("&&&&&", document.getElementById('bodyDiv'));
-        //
-        // document.getElementById('bodyDiv').style.marginTop = `${topBannerBottom}px`;
-    });
 
 
 
     return (
 
 
-                    <div className={`mx-4 text-center`}>
-                        <>
-                            <TopBanner className={`fixed-top mt-4 h-auto`} id={'t1'} src={IMAGES[2].url}/>
+        <div className={`mx-4 text-center`}>
+            <>
+                <TopBanner className={`fixed-top mt-4 h-auto`} id={'t1'} src={IMAGES[2].url}/>
 
-                        </>
-                        {/*<div className={`d-flex position-relative flex-column mt-5`}>*/}
-                        <div className={`d-flex position-relative flex-column`} id={'bodyDiv'}>
+            </>
+            <div className={`d-flex position-relative flex-column`} id={'bodyDiv'}>
 
-                            <img className={'loweLogo w-50  mb-5 pb-5 mx-auto '} src={'https://igeller.github.io/assets/Assets/Mobile/Logos/Africa-Mobile-Logo-Lowe.png'}/>
+                <img className={'loweLogo w-50  mb-5 pb-5 mx-auto '} src={'https://igeller.github.io/assets/Assets/Mobile/Logos/Africa-Mobile-Logo-Lowe.png'}/>
 
-                            <Textfit mode={'single'} className={`mt-5 pt-5 ${styles.title}`}>RE-PRESENTING ART</Textfit>
-                            <p className={'mx-auto my-3 font-italic'}>
-                                A Holographic Installation<br/>
-                                for the Lowe Art of<br/>
-                                Africa Gallery Collections
-                            </p>
-                            <Link href={'mask_gallery/'}>
-                                <Button className={`${styles.btn} mt-5 text-wrap badge-pill btn-lg px-3 py-2 w-75 mx-auto`}>View Gallery</Button>
-                            </Link>
-                        </div>
-                        {/*CHANGE TO CORRECT IMAGE BELOW!!*/}
-                        <img className={'mellonLogo w-50 mt-5 pt-5 mx-auto'} src={`${IMAGES[1].url}`}/>
+                <Textfit mode={'single'} className={`mt-5 pt-5 ${styles.title}`}>RE-PRESENTING ART</Textfit>
+                <p className={'mx-auto my-3 font-italic'}>
+                    A Holographic Installation<br/>
+                    for the Lowe Art of<br/>
+                    Africa Gallery Collections
+                </p>
+                <Link href={'mask_gallery/'}>
+                    <Button className={`${styles.btn} mt-5 text-wrap badge-pill btn-lg px-3 py-2 w-75 mx-auto`}>View Gallery</Button>
+                </Link>
+            </div>
+            {/*CHANGE TO CORRECT IMAGE BELOW!!*/}
+            <img className={'mellonLogo w-50 mt-5 pt-5 mx-auto'} src={`${IMAGES[1].url}`}/>
 
-                        <BottomBanner/>
-                    </div>
-
+            <BottomBanner/>
+        </div>
 
 
 
 
-  )
+
+    )
 }
