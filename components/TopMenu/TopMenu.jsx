f5b830afbb543032ad8a0c792f3284522c1ea74b
IG <ieg23@miami.edu>
2021-07-09 20:23:17 -0400

    added to navbar just need to fix the spacing

--- components/TopMenu/TopMenu.jsx
+++ components/TopMenu/TopMenu.jsx
@@ -4,45 +4,48 @@ import {Navbar} from "react-bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import Link from "next/link";
 import {BsImages} from "react-icons/bs";
-import { TiHome} from "react-icons/ti";
-
+import {TiHome} from "react-icons/ti";
+import styles from './TopMenu.module.scss'
 const ReactFitText = require('react-fittext');
 
 const TopMenu = (props) => {
     return(
         <>
             <Navbar variant={"dark"} expand={"3000px"} bg={"black"} className={'d-flex justify-content-around p-0 mt-4 mx-4'}>
-                <div className={`ml-0`}>
-                    <Link href={`/`} className={`align-self-left mx-0 `}>
+                <div className={`ml-0 ${styles.navGrid}`}>
+                    <Link href={`/`} className={`float-left mx-0 `}>
                         <div  className={`mx-0`}>
-                            <ReactFitText compressor={1.1} maxFontSize={20} minFontSize={18}>
-                                <h1>
-                                    <TiHome  className={`vertical-align-inherit mr-2`}/>
-                                    Representing Art
-                                </h1>
-                            </ReactFitText>
+                            <TiHome  className={`vertical-align-inherit mr-2`}/>
+                        
+                        </div>
+                    </Link>
+    
+    
+                    <div  className={`mx-0`}>
+                        <ReactFitText compressor={1.1} maxFontSize={20} minFontSize={18}>
+                            <h1>
+                                RePresenting ART
+                            </h1>
+                        </ReactFitText>
+                    </div>
+    
+    
+                    <Link href={`/mask_gallery`} className={`align-self-left mx-0 `}>
+                        <div  className={`justify-content-end`}>
+                            <BsImages className={`vertical-align-inherit mr-2`} style={{
+                                stroke: `white !important`,
+                                fill: `white !important`,
+                            }}
+                                      fill={`white`}
+                            />
                         </div>
                     </Link>
 
                 </div>
-    <div className={`mr-0`}>
-        <Link href={`/mask_gallery`} className={`align-self-left mx-0 `}>
-            <div  className={`mx-0`}>
-                <ReactFitText compressor={1.1} maxFontSize={20} minFontSize={18}>
-                    <h1>
-                        <BsImages className={`vertical-align-inherit mr-2`} style={{
-                            stroke: `white !important`,
-                            fill: `white !important`,
-                        }}
-                                  fill={`white`}
-                        />
-                        Gallery
-                    </h1>
-                </ReactFitText>
-            </div>
-        </Link>
-
-    </div>
+    {/*<div className={`mr-0`}>*/}
+    
+    
+    {/*</div>*/}
             
             </Navbar>
 
